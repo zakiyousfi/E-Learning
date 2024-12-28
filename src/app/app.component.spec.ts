6caa03bf814eda798384def2be9d6f161960a5a9
zaki <zakataka2004@gmail.com>
2024-12-29 00:10:04 +0100

    dist  commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'PrestigeTN'`, () => {
+  it(`should have as title 'E-Learning'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('PrestigeTN');
+    expect(app.title).toEqual('E-Learning');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('PrestigeTN app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('E-Learning app is running!');
   });
 });
